63054dc4c506d6bd458fda5ccfefe19907f2b334
FareedKhan-dev <whereismymind557@gmail.com>
2025-12-27 23:02:08 +0500

    tf: Output vars

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,26 @@
+# infra/terraform/outputs.tf
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster's API server."
+  value       = module.eks.cluster_endpoint
+}
+
+output "aurora_db_endpoint" {
+  description = "The writer endpoint for the Aurora PostgreSQL cluster."
+  value       = module.aurora.cluster_endpoint
+}
+
+output "redis_primary_endpoint" {
+  description = "The primary endpoint for the ElastiCache Redis cluster."
+  value       = aws_elasticache_replication_group.redis.primary_endpoint_address
+}
+
+output "s3_documents_bucket_name" {
+  description = "The name of the S3 bucket for document storage."
+  value       = aws_s3_bucket.documents.id
+}
